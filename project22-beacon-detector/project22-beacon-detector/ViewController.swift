f41c0af7b5fd4bd870bc32d22725702bfdc9b185
lsimon <leo@lsimon.org>
2024-08-14 15:24:19 -0400

    Day 76: Finish Beacon detector project and go through the challenges

--- project22-beacon-detector/project22-beacon-detector/ViewController.swift
+++ project22-beacon-detector/project22-beacon-detector/ViewController.swift
@@ -11,17 +11,41 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var distanceReading: UILabel!
+    @IBOutlet weak var distanceIndicator: UIView!
     
     var locationManager: CLLocationManager?
     
+    @IBOutlet weak var beaonDistanceSelect: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.locationManager = CLLocationManager()
         self.locationManager?.delegate = self
         self.locationManager?.requestAlwaysAuthorization()
+        self.distanceIndicator.layer.cornerRadius = 128
+        self.distanceIndicator.layer.zPosition = 0
+        self.distanceIndicator.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+        
+        self.distanceReading.layer.zPosition = 1
         
         self.view.backgroundColor = .gray
+        
+        let selectionHandler = {[weak self] (action: UIAction) in
+            let strToDistance: [String: CLProximity] = ["Unknown": .unknown, "Far": .far, "Near": .near, "Right here": .immediate]
+            
+            guard let distance = strToDistance[action.title] else { return }
+            
+            self?.update(distance: distance)
+            
+        }
+        let options = [
+            UIAction(title: "Unknown", image: nil, identifier: nil, discoverabilityTitle: nil, attributes: [], state: .off, handler: selectionHandler),
+            UIAction(title: "Far", image: nil, identifier: nil, discoverabilityTitle: nil, attributes: [], state: .off, handler: selectionHandler),
+            UIAction(title: "Near", image: nil, identifier: nil, discoverabilityTitle: nil, attributes: [], state: .off, handler: selectionHandler),
+            UIAction(title: "Right here", image: nil, identifier: nil, discoverabilityTitle: nil, attributes: [], state: .off, handler: selectionHandler)
+        ]
+        let btnRangeMenu = UIMenu(title: "Hello", image: nil, identifier: nil, options: [], children: options)
+        self.beaonDistanceSelect.menu = btnRangeMenu;
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -57,15 +81,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             case .unknown:
                 self.view.backgroundColor = .gray
                 self.distanceReading.text = "UNKNOWN"
-            case .immediate:
+                self.distanceIndicator.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
+            case .far:
                 self.view.backgroundColor = .blue
                 self.distanceReading.text = "FAR"
+                self.distanceIndicator.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
             case .near:
                 self.view.backgroundColor = .orange
                 self.distanceReading.text = "NEAR"
-            case .far:
+                self.distanceIndicator.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            case .immediate:
                 self.view.backgroundColor = .red
                 self.distanceReading.text = "RIGHT HERE"
+                self.distanceIndicator.transform = CGAffineTransform(scaleX: 1, y: 1)
             default:
                 self.view.backgroundColor = .gray
                 self.distanceReading.text = "UNKNOWN"
